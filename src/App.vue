c83a629cf18cc75a36889b9c725af80ba2a3b73b
Bagus Setyo <bagussetyo442@gmail.com>
2023-11-06 15:43:23 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,28 @@
       class="max-w-screen-lg w-80 sm:w-full mx-auto m-4 bg-white shadow-lg rounded-2xl sm:p-12 p-8 text-center sm:flex justify-center items-center"
     >
       <div class="flex-1">
-        <img src="/images/illustration-woman-online-desktop.svg" alt="" />
+        <img
+          class="static sm:block hidden overflow-hidden women"
+          src="/images/illustration-woman-online-desktop.svg"
+          alt=""
+        />
+        <img
+          class="static block sm:hidden overflow-hidden womens"
+          src="/images/illustration-woman-online-mobile.svg"
+          alt=""
+        />
+        <img
+          class="absolute sm:block hidden bg"
+          src="/images/bg-pattern-desktop.svg"
+        />
+        <img
+          class="absolute block sm:hidden bgs"
+          src="/images/bg-pattern-mobile.svg"
+        />
+        <img
+          class="static sm:block hidden overflow-hidden box"
+          src="/images/illustration-box-desktop.svg"
+        />
       </div>
       <div class="ml-4 flex-1">
         <h1
@@ -96,5 +117,21 @@ export default {
 .unrotated {
   transition: transform 0.5s ease;
   transform: rotate(0deg);
+}
+.bg {
+  transform: translate(-60%, -72%);
+}
+.bgs {
+  position: inherit;
+}
+.women {
+  transform: translateX(-25%);
+}
+.womens {
+  position: absolute;
+  transform: translateY(-60%);
+}
+.box {
+  transform: translate(-80%, -95%);
 }
 </style>
