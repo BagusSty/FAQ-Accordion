8c70cf43138a9bceb8d3acf7c2045b60e5e1f103
Bagus Setyo <bagussetyo442@gmail.com>
2023-11-06 13:55:28 +0700

    add font-awesome & update css

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,48 @@
 <template>
-  <!-- Your existing code -->
-  <div
-    class="max-w-screen-lg mx-auto m-4 bg-white shadow-lg rounded-lg p-8 text-center flex justify-center items-center"
-  >
-    <div class="flex-1">
-      <img src="images/illustration-woman-online-desktop.svg" alt="" />
-    </div>
-    <div class="ml-4 flex-1">
-      <h1
-        class="font-bold text-left text-4xl text-veryDarkDesaturatedBlue mb-5"
-      >
-        FAQ
-      </h1>
-      <div
-        v-for="(faq, index) in faqs"
-        :key="index"
-        class="text-veryDarkGrayishBlue mb-2"
-      >
-        <div class="flex justify-between items-center">
-          <p
-            class="hover:text-softRed hover:cursor-pointer"
-            @click="toggleAnswer(index)"
-          >
-            {{ faq.question }}
-          </p>
-          <img
-            v-if="faq.showAnswer"
-            class="w-2 hover:cursor-pointer"
-            src="images/icon-arrow-up.svg"
-            alt=""
-          />
-          <img
-            v-else
-            class="w-2 hover:cursor-pointer"
-            src="images/icon-arrow-down.svg"
-            alt=""
-          />
+  <div class="flex justify-center items-center h-screen">
+    <div
+      class="max-w-screen-lg w-80 sm:w-full mx-auto m-4 bg-white shadow-lg rounded-2xl sm:p-12 p-8 text-center sm:flex justify-center items-center"
+    >
+      <div class="flex-1">
+        <img src="images/illustration-woman-online-desktop.svg" alt="" />
+      </div>
+      <div class="ml-4 flex-1">
+        <h1
+          class="font-bold sm:text-left text-center text-4xl text-veryDarkDesaturatedBlue mb-5"
+        >
+          FAQ
+        </h1>
+        <div
+          v-for="(faq, index) in faqs"
+          :key="index"
+          class="text-veryDarkGrayishBlue mb-2"
+        >
+          <div class="flex justify-between items-center">
+            <p
+              class="hover:text-softRed hover:cursor-pointer"
+              @click="toggleAnswer(index)"
+              v-bind:class="{ 'font-bold': faq.showAnswer }"
+            >
+              {{ faq.question }}
+            </p>
+            <font-awesome-icon
+              v-if="faq.showAnswer"
+              class="hover:cursor-pointer text-softRed"
+              :icon="['fas', 'chevron-up']"
+            />
+            <font-awesome-icon
+              v-else
+              class="hover:cursor-pointer text-softRed"
+              :icon="['fas', 'chevron-down']"
+            />
+          </div>
+          <div v-if="faq.showAnswer" class="faq-answer">
+            <p class="font-thin text-left text-darkGrayishBlue py-2">
+              {{ faq.answer }}
+            </p>
+          </div>
+          <hr class="my-2" />
         </div>
-        <div v-if="faq.showAnswer" class="faq-answer">{{ faq.answer }}</div>
       </div>
     </div>
   </div>
@@ -62,17 +67,19 @@ export default {
         {
           question: " How do I reset my password?",
           answer:
-            "Please enter your new password, and then try again later to login again later to complete your registration",
+            "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
           showAnswer: false,
         },
         {
           question: "Can I cancel my subscription?",
-          answer: "",
+          answer:
+            "Yes! Send us a message and we’ll process your request no questions asked.",
           showAnswer: false,
         },
         {
           question: "Do you provide additional support?",
-          answer: "",
+          answer:
+            "Chat and email support is available 24/7. Phone lines are open during normal business hours",
           showAnswer: false,
         },
       ],
